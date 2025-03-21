cbe645485be1ec1bcc596b3de7e78ba2686683c8
RaphaelAubry <raphael.aubry@hotmail.fr>
2025-03-21 16:15:07 +0100

    progression

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,13 @@
 class Project < ApplicationRecord
   has_many :tasks, dependent: :destroy
+
+  def progression
+    begin
+      tasks.present? ? tasks.finished.length / tasks.length.to_f : 0
+    rescue Exception => e
+      Rails.logger.debug "Exception class : #{e.class}"
+      Rails.logger.debug "Exception message : #{e.message}"
+      Rails.logger.debug "Exception backtrace : \n#{e.backtrace.join("\n")}"
+    end
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,8 +3,9 @@ class Task < ApplicationRecord
 
   scope :available, ->() { where(user_id: nil) }
   scope :status, ->(value) { where(status: value) }
+  scope :finished, ->() { where(status: "terminée") }
 
-  STATUSES = [ "en cours", "terminée", "annulée", "stoppée", "aucun" ]
+  STATUSES = [ "en cours", "terminée", "annulée", "stoppée", "aucun" ].freeze
 
   def user
     User.find(user_id) if user_id
